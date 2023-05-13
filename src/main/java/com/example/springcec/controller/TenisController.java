7fb5c16212854c0cef7d0bd94e95d7b2e63c6969
Mathruu <matheusmathruu@gmail.com>
2023-05-13 13:58:00 -0300

    fix: regras de negocio

--- src/main/java/com/example/springcec/controller/TenisController.java
+++ src/main/java/com/example/springcec/controller/TenisController.java
@@ -5,11 +5,47 @@ import com.example.springcec.dominio.Tenis;
 public class TenisController {
 
     public boolean isTenisValido(Tenis tenis) {
-        return isModeloValido(tenis);
+        return isModeloValido(tenis) && isMarcaValida(tenis) && isCorValida(tenis) && isTamanhoValido(tenis)
+                && isPrecoValido(tenis) && isQuantidadeValida(tenis);
     }
 
     private boolean isModeloValido(Tenis tenis) {
-        if ((tenis.getModelo().isEmpty()) || (tenis.getMarca().isEmpty())) {
+        if ((tenis.getModelo().isEmpty())) {
+            return false;
+        }
+        return true;
+    }
+
+    private boolean isMarcaValida(Tenis tenis) {
+        if ((tenis.getMarca().isEmpty())) {
+            return false;
+        }
+        return true;
+    }
+
+    private boolean isCorValida(Tenis tenis) {
+        if ((tenis.getCor().isEmpty())) {
+            return false;
+        }
+        return true;
+    }
+
+    private boolean isTamanhoValido(Tenis tenis) {
+        if ((tenis.getTamanho().isEmpty())) {
+            return false;
+        }
+        return true;
+    }
+
+    private boolean isPrecoValido(Tenis tenis) {
+        if ((tenis.getPreco() == null)) {
+            return false;
+        }
+        return true;
+    }
+
+    private boolean isQuantidadeValida(Tenis tenis) {
+        if ((tenis.getQuantidade() < 0)) {
             return false;
         }
         return true;
